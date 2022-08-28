498f3ed48134dda590b82b39a8b102fb4ce0d124
waterfog9580 <waterfog9580@gmail.com>
2022-08-28 19:37:42 +0900

    :sparkles: User 엔티티 구현

---
+++ src/main/java/com/waterfogsw/oauth2redis/user/entity/Role.java
@@ -0,0 +1,5 @@
+package com.waterfogsw.oauth2redis.user.entity;
+
+public enum Role {
+  USER
+}
---
+++ src/main/java/com/waterfogsw/oauth2redis/user/entity/User.java
@@ -0,0 +1,61 @@
+package com.waterfogsw.oauth2redis.user.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Length;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.lang.Assert;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Length(min = 1, max = 200)
+  private String username;
+
+  private String profileImgUrl;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @Builder
+  public User(
+      String username,
+      String profileImgUrl,
+      Role role
+  ) {
+    Assert.hasText(username, "Username must be provided");
+    Assert.notNull(role, "Role must be provided");
+
+    this.username = username;
+    this.profileImgUrl = profileImgUrl;
+    this.role = role;
+  }
+
+  public Claims getClaims() {
+    String sub = String.valueOf(getId());
+    Claims claims = Jwts
+        .claims()
+        .setSubject(sub);
+
+    claims.put("roles", getRole());
+    return claims;
+  }
+
+}
