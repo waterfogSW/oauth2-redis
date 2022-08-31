98147a627b65119c8d588fe5c94d08efd3ddc541
waterfog9580 <waterfog9580@gmail.com>
2022-08-31 17:16:53 +0900

    :white_check_mark: UserService signup 테스트 작성

--- src/test/java/com/waterfogsw/oauth2redis/user/service/UserServiceTest.java
+++ src/test/java/com/waterfogsw/oauth2redis/user/service/UserServiceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -65,6 +67,63 @@ class UserServiceTest {
 
         //then
         assertThat(foundUser.getId()).isEqualTo(userId);
+      }
+
+    }
+
+  }
+
+  @Nested
+  @DisplayName("signup 메서드는")
+  class DescribeSignUp {
+
+    @Nested
+    @DisplayName("username에 빈값이 전달되면")
+    class ContextWithUsernameNullOrEmpty {
+
+      @ParameterizedTest
+      @NullAndEmptySource
+      @DisplayName("IllegalArgumentException 에러를 발생시킨다")
+      void ItThrowsIllegalArgumentException(String src) {
+        //when, then
+        assertThatThrownBy(() -> userService.signup(src, "test.url"))
+            .isInstanceOf(IllegalArgumentException.class);
+      }
+
+    }
+
+    @Nested
+    @DisplayName("profileImgUrl에 빈값이 전달되면")
+    class ContextWithProfileImgUrlNullOrEmpty {
+
+      @ParameterizedTest
+      @NullAndEmptySource
+      @DisplayName("IllegalArgumentException 에러를 발생시킨다")
+      void ItThrowsIllegalArgumentException(String src) {
+        //when, then
+        assertThatThrownBy(() -> userService.signup("testname", src))
+            .isInstanceOf(IllegalArgumentException.class);
+      }
+
+    }
+
+    @Nested
+    @DisplayName("모든 값이 정상적으로 전달되면")
+    class ContextWithValidParameterPassed {
+
+      @Test
+      @DisplayName("user를 생성하여 저장한다")
+      void ItCreateUserAndSave() {
+        //given
+        String username = "test";
+        String profileImgUrl = "testImageUrl";
+
+        //when
+        userService.signup(username, profileImgUrl);
+
+        //then
+        verify(userRepository).save(any(User.class));
+
       }
 
     }
