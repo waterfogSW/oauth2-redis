883f4be8db01b0b34199a0b0305cf434ba76e59b
waterfog9580 <waterfog9580@gmail.com>
2022-09-18 23:29:22 +0900

    :sparkles: 유저 조회 메서드 구현

--- src/main/java/com/waterfogsw/oauth2redis/user/repository/UserRepository.java
+++ src/main/java/com/waterfogsw/oauth2redis/user/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.waterfogsw.oauth2redis.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.waterfogsw.oauth2redis.user.entity.Provider;
 import com.waterfogsw.oauth2redis.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByProviderAndProviderId(
+      Provider provider,
+      String providerId
+  );
 
 }
